591850fe25ea9e82b4593637b3873afe660db433
edwinacos55 <edwinacos55@gmail.com>
2022-05-22 17:45:00 -0400

    adding our handler to our component hierchy

--- src/fcfs.js
+++ src/fcfs.js
@@ -4,6 +4,9 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 class Fcfs extends Component {
     constructor(props) {
 		super(props);
+        this.state ={};
+        this.handle_calculate = this.handle_calculate.bind(this);
+
     }
 
     findWaitingTime(processes,n,burstTime,waitTime){
