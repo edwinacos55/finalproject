5f23aaed8ea78e7ef9ae9399c97835543ccd47e4
edwinacos55 <edwinacos55@gmail.com>
2022-05-22 13:20:24 -0400

    added components to app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Fcfs from "./Components/fcfs";
+import Sjf from "./Components/sjf";
+import Srtf from "./Components/srtf";
+import RR from "./Components/rr";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class  App extends Component() {
 }
+
 
 export default App;
