f14b7a033e94006d80334a40a74a97efedeeba24
edwinacos55 <edwinacos55@gmail.com>
2022-05-22 18:31:23 -0400

    adding constructors and setting state for round robin component

--- src/rr.js
+++ src/rr.js
@@ -2,7 +2,10 @@ import React, { Component } from "react";
 import { Row, Col,Button, Container} from "reactstrap";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 class RR extends Component {
-    
+    constructor(props) {
+		super(props);
+		this.state = {};
+    }
 }
 
 export default RR
