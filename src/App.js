162913db5e878313c6c79387e948f11fbaa1463e
edwinacos55 <edwinacos55@gmail.com>
2022-05-22 14:14:24 -0400

    installed react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
-import { Row, Col,Button, Container} from "reactstrap";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Fcfs from "./fcfs";
 import Sjf from "./sjf";
 import Srtf from "./srtf";
 import RR from "./rr";
+import Home from "./Home";
 
 
 class  App extends Component() {
   constructor(props) {
 		super(props);
-		this.state = {};
-}
-}
+		this.state = {};}
+  }
 
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -5,28 +5,29 @@ class Home extends Component {
 		super(props);
 		this.state = {};
     }
-    render() {
-		return (
-			<React.Fragment>
-				<h1>CPU Scheduling Algorithms Calculator</h1>
-			  <Container fluid>
-				<div className="container-mg mb-4">
-					<Row><Col md={12}>
-                        <Link to="/fcfs">FCFS</Link>
-                    </Col></Row>
-                    <Row><Col md={12}>
-                        <Link to="/sjf">SJF</Link>
-                    </Col></Row>
-                    <Row><Col md={12}>
-                        <Link to="/rr">Round Robin</Link>
-                    </Col></Row>
-                    <Row><Col md={12}>
-                        <Link to="/srtf">SRTF</Link>
-                    </Col></Row>
-				</div>
-				</Container>
-			</React.Fragment>
-		);
-	}
+    // render() {
+	// 	return (
+	// 		<React.Fragment>
+	// 			<h1>CPU Scheduling Algorithms Calculator</h1>
+	// 		  <Container fluid>
+	// 			<div className="container-mg mb-4">
+	// 				<Row><Col md={12}>
+    //                     <Link to="/fcfs">FCFS</Link>
+    //                 </Col></Row>
+    //                 <Row><Col md={12}>
+    //                     <Link to="/sjf">SJF</Link>
+    //                 </Col></Row>
+    //                 <Row><Col md={12}>
+    //                     <Link to="/rr">Round Robin</Link>
+    //                 </Col></Row>
+    //                 <Row><Col md={12}>
+    //                     <Link to="/srtf">SRTF</Link>
+    //                 </Col></Row>
+	// 			</div>
+	// 			</Container>
+	// 		</React.Fragment>
+	// 	);
+	// }
 }
+
 export default Home
