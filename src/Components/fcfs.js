eba779777ae9a6f3f7ca619633f1da78fbdba2a8
edwinacos55 <edwinacos55@gmail.com>
2022-05-22 13:27:26 -0400

    added logic to find waiting time on fcfs.js

--- src/Components/fcfs.js
+++ src/Components/fcfs.js
@@ -1,5 +1,16 @@
 import React, { Component } from "react";
 class Fcfs extends Component {
-}
+    constructor(props) {
+		super(props);
+    }
 
+    findWaitingTime(processes,n,burstTime,waitTime){
+        waitTime[0] = 0;
+        for (let i = 1; i < n; i++) {
+            waitTime[i] = burstTime[i - 1] + waitTime[i - 1];
+        }
+    }
+
+
+}
 export default Fcfs
