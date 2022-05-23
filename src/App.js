d1cffcfeaea7d487bc5e883ef37c989b399d5764
edwinacos55 <edwinacos55@gmail.com>
2022-05-23 00:00:21 -0400

    changed home.js to try to render again

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import React, { Component } from "react";
-import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Fcfs from "./fcfs";
 import Sjf from "./sjf";
-import Srtf from "./srtf";
 import RR from "./rr";
+import Srtf from "./srtf";
 import Home from "./home";
 
-
-class  App extends Component() {
-  constructor(props) {
+class App extends Component {
+	constructor(props) {
 		super(props);
-		this.state = {};}
-    render() {
-      return (
-        <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="fcfs" element={<Fcfs />} />
-          <Route path="sjf" element={<Sjf />} />
-          <Route path="rr" element={<RR />} />
-          <Route path="srtf" element={<Srtf />} />				
-        </Routes>
-        </BrowserRouter>
-      );
-    }
-  }
+		this.state = {
+		};
+	}
 
-export default App;
+	render() {
+		return (
+			<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="fcfs" element={<Fcfs />} />
+				<Route path="sjf" element={<Sjf />} />
+				<Route path="rr" element={<RR />} />
+				<Route path="srtf" element={<Srtf />} />				
+			</Routes>
+			</BrowserRouter>
+		);
+	}
+}
+
+export default App
--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,22 @@
 import React, { Component } from "react";
 import { Row, Col, Container } from "reactstrap";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 class Home extends Component {
-    constructor(props) {
+	constructor(props) {
 		super(props);
 		this.state = {};
-    }
+	}
 
 
-    render() {
-        const newLocal = <Link to="/fcfs">FCFS</Link>;
+	render() {
 		return (
 			<React.Fragment>
 				<h1>CPU Scheduling Algorithms Calculator</h1>
 			  <Container fluid>
 				<div className="container-mg mb-4">
-                <Row><Col md={12}>
+					<Row><Col md={12}>
                         <Link to="/fcfs">FCFS</Link>
                     </Col></Row>
                     <Row><Col md={12}>
@@ -33,6 +33,6 @@ class Home extends Component {
 			</React.Fragment>
 		);
 	}
- }
+}
 
 export default Home
