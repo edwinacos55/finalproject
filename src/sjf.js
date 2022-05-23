1868ad330bddd6d7cc97bb7a0dbe065df68b5c26
edwinacos55 <edwinacos55@gmail.com>
2022-05-22 21:24:44 -0400

    added logic to calculate waitingTime and turn around time in SJF algo cmponent

--- src/sjf.js
+++ src/sjf.js
@@ -7,5 +7,16 @@ class Sjf extends Component {
 		this.state = {
 		};
     }
+    findWaitingTime(processes,n,burstTime,waitTime){
+        waitTime[0] = 0;
+        for (let i = 1; i < n; i++) {
+            waitTime[i] = burstTime[i - 1] + waitTime[i - 1];
+        }
+    }
+    findTurnAroundTime(processes,n,burstTime,waitTime,turnAroundTime){
+        for (let i = 0; i < n; i++) {
+            turnAroundTime[i] = burstTime[i] + waitTime[i];
+        }
+    }
 }
 export default Sjf
