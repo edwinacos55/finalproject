b9544ef0ddeb6a55ed1fb6c7f7c6debca7d3ca41
edwinacos55 <edwinacos55@gmail.com>
2022-05-23 20:22:52 -0400

    added contructor, props ans set state

--- src/srtf.js
+++ src/srtf.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import { Row, Col,Button, Container} from "reactstrap";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 class Srtf extends Component {
+    constructor(props) {
+		super(props);
+		this.state = {
+		};
+    }
 }
 
 export default Srtf
