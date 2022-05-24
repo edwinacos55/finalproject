6fe592822427dc46a6c8fa2a33d2b84b918a0735
edwinacos55 <edwinacos55@gmail.com>
2022-05-23 23:04:42 -0400

    added Turn Around Time to display next to Waiting time for each procces entered

--- src/fcfs.js
+++ src/fcfs.js
@@ -46,7 +46,7 @@ class Fcfs extends Component {
         for (let i = 0; i < n; i++) {
 			var colorcn = this.chartcolors.length;
             var color_index = i % colorcn;
-			html += `<span> ${processes[i]} waiting time = ${waitTime[i]} </span><br>`;
+			html += `<span> ${processes[i]} Waiting Time = ${waitTime[i]} || Turn Around Time= ${turnAroundTime[i]}  </span><br>`;
 			let width = (100 * burstTime[i] / total_burstTime).toFixed(2);
 			chart += `<div class="d-flex" style="width: ${width}%;background-color:${this.chartcolors[color_index]}"></div>`;
 			chartdesp += `<div class="d-flex" style="width: ${width}%;">${processes[i]}(${burstTime[i]})</div>`;
